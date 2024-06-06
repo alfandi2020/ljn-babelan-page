02294cb995e3fc32b766bb88dbd2e4c47f107199
alfandi2020 <pandibl4530@gmail.com>
2024-06-06 21:50:00 +0700

    a

--- application/views/id/index.php
+++ application/views/id/index.php
@@ -1232,7 +1232,12 @@ Blok F4 No 9
 Babelan Kota, Kec Babelan, Kab Bekasi
 Jawa Barat 17610 <br> 081933803366
 <br>mahfud612@gmail.com</p> -->
-							<p>CV FAN TECNO MEDIA <br>PLAZA SIMATUPANG LAANTAI 6 UNIT 3 JALAN TB SIMATUPANG KAVLING IS NOMOR 01,Desa/Kelurahan Pondok pinang,kec.kebayoran lama,kota adm.Jakarta Selatan,Provinsi DKI Jakarta Kode Pos 12310
+							<p>Lintas Jaringan Nusantara
+Kantor Layanan Babelan <br>Perum Wahana Pondok Ungu 
+Blok F4 No 9
+Babelan Kota, Kec Babelan, Kab Bekasi
+Jawa Barat 17610
+
 <br> 081933803366
 <br>mahfud612@gmail.com</p>
 						</div>
